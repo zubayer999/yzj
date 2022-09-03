8bd652d2d9850dc1f661fdc62359ec47af953252
zubayer999 <jubayer.hasan1991@gmail.com>
2022-09-03 18:25:26 +0600

    upload version0.1

---
+++ js/theme.js
@@ -0,0 +1,113 @@
+// js Document
+
+
+
+(function($) {
+    "use strict";
+
+// ----------------------------- Counter Function
+        var timer = $('.counter');
+        if(timer.length) {
+          $('.counter').counterUp({
+            delay: 10,
+            time: 1200,
+          });
+        }
+
+// ------------------------ Navigation Scroll
+        $(window).on('scroll', function (){   
+          var sticky = $('.sticky-menu'),
+          scroll = $(window).scrollTop();
+          if (scroll >= 100) sticky.addClass('fixed');
+          else sticky.removeClass('fixed');
+
+        });
+
+// -------------------- Remove Placeholder When Focus Or Click
+        $("input,textarea").each( function(){
+            $(this).data('holder',$(this).attr('placeholder'));
+            $(this).on('focusin', function() {
+                $(this).attr('placeholder','');
+            });
+            $(this).on('focusout', function() {
+                $(this).attr('placeholder',$(this).data('holder'));
+            });     
+        });
+
+        
+// --------------------------------- Contact Form
+          // init the validator
+          // validator files are included in the download package
+          // otherwise download from http://1000hz.github.io/bootstrap-validator
+
+        if($("#contact-form").length) {
+            $('#contact-form').validator();
+            // when the form is submitted
+            $('#contact-form').on('submit', function (e) {
+
+                // if the validator does not prevent form submit
+                if (!e.isDefaultPrevented()) {
+                    var url = "inc/sneml.php";
+
+                    // POST values in the background the the script URL
+                    $.ajax({
+                        type: "POST",
+                        url: url,
+                        data: $(this).serialize(),
+                        success: function (data)
+                        {
+                            // data = JSON object that sneml.php returns
+
+                            // we recieve the type of the message: success x danger and apply it to the
+                            var messageAlert = 'alert-' + data.type;
+                            var messageText = data.message;
+
+                            // let's compose Bootstrap alert box HTML
+                            var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+
+                            // If we have messageAlert and messageText
+                            if (messageAlert && messageText) {
+                                // inject the alert to .messages div in our form
+                                $('#contact-form').find('.messages').html(alertBox);
+                                // empty the form
+                                $('#contact-form')[0].reset();
+                            }
+                        }
+                    });
+                    return false;
+                }
+            });
+          }
+
+
+
+$(window).on ('load', function (){ // makes sure the whole site is loaded
+
+// -------------------- Site Preloader
+        $('#ctn-preloader').fadeOut(); // will first fade out the loading animation
+        $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({'overflow':'visible'});
+
+   
+// ------------------------------------- Fancybox
+        var fancy = $ (".fancybox");
+        if(fancy.length) {
+          fancy.fancybox({
+            arrows: true,
+            buttons: [
+              "zoom",
+              //"share",
+              "slideShow",
+              //"fullScreen",
+              //"download",
+              "thumbs",
+              "close"
+            ],
+            animationEffect: "zoom-in-out",
+            transitionEffect: "zoom-in-out",
+          });
+        }
+
+    });  //End On Load Function
+    
+})(jQuery);
